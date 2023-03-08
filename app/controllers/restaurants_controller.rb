845ee438ef4194484056452e60b92966c73c9992
weng haishi <wenghaishi1@gmail.com>
2023-03-08 15:10:48 +0800

    implemented destroy method for restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,7 +9,24 @@ class RestaurantsController < ApplicationController
   end
 
   def new
-    @restaurant
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to restaurants_path
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+
+    redirect_to restaurants_path
   end
   
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
